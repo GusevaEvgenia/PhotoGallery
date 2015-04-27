74a8987993445908b5cdbdfeb1030c768e265144
Belka <Evgenia_@email.ua>
2015-04-27 11:30:18 +0300

    fragments structure

---
+++ app/src/main/java/com/android_lessons/belkin/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,9 @@
+package com.android_lessons.belkin.photogallery;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Belkin on 27.04.2015.
+ */
+public class PhotoGalleryFragment extends Fragment {
+}
